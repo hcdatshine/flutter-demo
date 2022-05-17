730f9458a248ea1487bfef467bada2ba2df62641
Vu Quoc Dat <datvuquocbk@gmail.com>
2022-05-17 10:26:40 +0900

    return message json type

--- server.js
+++ server.js
@@ -6,48 +6,27 @@ const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 app.listen(port);
 
-
-console.log('todo list RESTful API server started on: ' + port);
-
 app.get('/test', (req, res) => {
-    res.send('Hello World');
-	// var con = mysql.createConnection(db);
-	
-	// con.connect(function(err) {
-		
-	// 	if (err) throw err;
-	// 	console.log("Connected!");
-
-	// 	var id = req.param('id');
-	// 	var where = id == 'all' ? '' : ' WHERE id = ' + id;
-	  
-	// 	con.query("SELECT * FROM articles" + where, function (err, result, fields) {
-	// 		if (err) throw err;
-	// 		res.send(result);
-	// 		con.end();
-	// 	});
-		
-	// });
-	
+  res.json({'message':'Hello World'});
 });
 
-// post
+const dotenv = require('dotenv')
+dotenv.config()
+let current_url = process.env.NODE_ENV === 'development' ? process.env.local_url:process.env.server_url
 
 app.post('/upload', function(req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).json({'error': 'No files were uploaded.'});
   }
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.file;
-  let time = Date.now();
-
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(`/home/ec2-user/flutter-demo/image/${sampleFile.name}`, function(err) {
-//   sampleFile.mv(`/Users/datshinemac/Desktop/node-ec2/image/${sampleFile.name}`, function(err) {
-    if (err)
-      return res.status(500).send(err);
+  sampleFile.mv(`${current_url}${sampleFile.name}`, function(err) {
+    // return json
+    if (err){
+      return res.status(500).json({'error': 'No files were uploaded.'});
+    }
     else {
-        res.status(200).send('File uploaded!');
+      res.status(200).json({'success':'File uploaded!'});
     }
   });
 });
